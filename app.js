691b70560b505a5191cad8b7cc6fd198e24db547
karanysingh <56989043+karanysingh@users.noreply.github.com>
2021-10-20 19:40:53 +0530

    .

--- app.js
+++ app.js
@@ -1,45 +1,70 @@
 //jshint esversion6
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-const axios = require('axios');
-const { response } = require('express');
+// const axios = require('axios');
+// const { response } = require('express');
+const Http = new XMLHttpRequest();
+const url='https://arduinoiotbot.herokuapp.com/';
 
-const getData=document.getElementsByClassName('btn');
-const upBtn= document.getElementsByClassName('up');
-const downBtn= document.getElementsByClassName('down');
-const leftBtn= document.getElementsByClassName('left');
-const rightBtn= document.getElementsByClassName('right');
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+  document.getElementById('status').innerText="Status: "+Http.responseText;
+
+}
+
+// const getData= document.getElementsByClassName('fetch')[0];
+const upBtn= document.getElementsByClassName('up')[0];
+const downBtn= document.getElementsByClassName('down')[0];
+const leftBtn= document.getElementsByClassName('left')[0];
+const rightBtn= document.getElementsByClassName('right')[0];
 
 
-const getData = () =>{
-    axios.get('https://arduinoiotbot.herokuapp.com/').then(response=>{
-        console.log(response);
-    });
+const getDataServer = () =>{
+    // axios.get('https://arduinoiotbot.herokuapp.com/').then(response=>{
+    //     console.log(response);
+    // });
+    Http.open("GET", url);
+    Http.send()
 };
 
 const sendupData=()=>{
-    axios.post('https://arduinoiotbot.herokuapp.com/modify?move=w').then(response=>{
-        console.log(response);
-    });
+    // axios.get('https://arduinoiotbot.herokuapp.com/modify?move=w').then(response=>{
+    //     console.log(response);
+    // });
+    getDataServer()
+    
+    Http.open("GET", url+"/modify?move=w");
+    Http.send();
 };
 const sendDownData=()=>{
-    axios.post('https://arduinoiotbot.herokuapp.com/modify?move=s').then(response=>{
-        console.log(response);
-    });
+    // axios.get('https://arduinoiotbot.herokuapp.com/modify?move=s').then(response=>{
+    //     console.log(response);
+    // });
+    getDataServer()
+    
+    Http.open("GET", url+"/modify?move=s");
+    Http.send();
 };
 const sendLeftData=()=>{
-    axios.post('https://arduinoiotbot.herokuapp.com/modify?move=a').then(response=>{
-        console.log(response);
-    });
+    // axios.get('https://arduinoiotbot.herokuapp.com/modify?move=a').then(response=>{
+    //     console.log(response);
+    // });
+    getDataServer()
+    
+    Http.open("GET", url+"/modify?move=a");
+    Http.send();
 };
 const sendRightData=()=>{
-    axios.post('https://arduinoiotbot.herokuapp.com/modify?move=d').then(response=>{
-        console.log(response);
-    });
+    // axios.get('https://arduinoiotbot.herokuapp.com/modify?move=d').then(response=>{
+    //     console.log(response);
+    // });
+    getDataServer()
+
+    Http.open("GET", url+"/modify?move=d");
+    Http.send();
 };
-getData.addEventListener('click',getData);
 upBtn.addEventListener('click',sendupData);
 downBtn.addEventListener('click',sendDownData);
 leftBtn.addEventListener('click',sendLeftData);
